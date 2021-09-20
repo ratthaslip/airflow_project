e8f578c906703b88840f84033681db7bf5d03645
ratthaslip <89380151+ratthaslip@users.noreply.github.com>
2021-09-20 20:31:48 +0700

    Create directory.py

---
+++ directory.py
@@ -0,0 +1,31 @@
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+
+yesterday = datetime.datetime.combine(
+   datetime.datetime.today() - datetime.timedelta(1),
+   datetime.datetime.min.time())
+
+default_dag_args = {
+     'owner': 'airflow',
+    'start_date': yesterday,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+}
+
+dag = DAG(
+        ‘Create test_dir’,
+         default_args=default_dag_args)
+
+
+t1 = BashOperator(
+	task_id=’Make directory’, bash_command=’mkdir  ’, dag=dag)
+
+
+t2 = BashOperator(
+	task_id=’Show directory’, bash_command=’ ’, dag=dag)
+
+t1 >> t2   # This is how we set dependency among two tasks
