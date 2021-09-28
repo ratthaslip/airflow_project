a894ae22731f4f9ad3620418d7d5c6cf71b8c8f2
ratthaslip <89380151+ratthaslip@users.noreply.github.com>
2021-09-29 00:12:45 +0700

    Create covid19_get_data.py

---
+++ covid19_get_data.py
@@ -0,0 +1,35 @@
+import json
+from datetime import datetime
+
+from airflow import DAG
+from airflow.hooks.mysql_hook import MySqlHook
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.utils.dates import days_ago
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2020, 7, 1),
+    'provide_context': True  # to support task instance for XComms with kwargs['ti']
+}
+
+with DAG('covid19_get_data',
+         schedule_interval='@daily',
+         default_args=default_args,
+         description='A simple data pipeline for COVID-19 report',
+         catchup=True) as dag:
+
+    t1 = SimpleHttpOperator(
+        task_id='get_covid19_report_today',
+        method='GET',
+        http_conn_id='https_covid19_api',
+        endpoint='/api/Cases/today-cases-all',
+        headers={"Content-Type":"application/json"},
+        dag=dag
+    )
+
+
+    t1 
