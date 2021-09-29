d8f3acdc261782b197520faa65d88ab401b8b93b
ratthaslip <89380151+ratthaslip@users.noreply.github.com>
2021-09-29 11:11:56 +0700

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,16 +1,13 @@
 USE covid19;
 CREATE TABLE daily_covid19_reports (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  confirmed INT(6),
-  recovered INT(6),
-  hospitalized INT(6),
-  deaths INT(6),
-  new_confirmed INT(6),
-  new_recovered INT(6),
-  new_hospitalized INT(6),
-  new_deaths INT(6),
-  update_date DATETIME,
-  source VARCHAR(100),
-  dev_by VARCHAR(100),
-  server_by VARCHAR(100)
-  );
+  txn_date DATETIME,
+  new_case INT(10),
+  total_case INT(10),
+  total_case_excludeabroad INT(10),
+  new_death INT(10),
+  total_death INT(10),
+  new_recovered INT(10),
+  total_recovered" INT(10),
+  update_date DATETIME
+);
